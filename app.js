845846da5cc66dd57cf0f5524066f46df6f6621c
adedaporh <adedaporh@outlook.com>
2020-11-10 07:06:59 +0100

    Adding 'Game over' screen

--- app.js
+++ app.js
@@ -7,8 +7,29 @@ const app = Vue.createApp({
         return{
             playerHealth: 100,
             monsterHealth:100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null,
         };
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                //draw
+                this.winner = "draw";
+            } else if (value <= 0) {
+                // player lost
+                this.winner = "monster";
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                //draw
+                this.winner = "draw";
+            } else if (value <= 0) {
+                // monster lost
+                this.winner = "player";
+            }
+        },
     },
     computed: {
         monsterBarStyles() {
