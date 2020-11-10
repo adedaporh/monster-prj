958841921993924c0baac4a620e1149544acfb3f
adedaporh <adedaporh@outlook.com>
2020-11-10 08:13:56 +0100

    Adding battle log

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
             monsterHealth:100,
             currentRound: 0,
             winner: null,
+            logMessages: [],
         };
     },
     watch: {
@@ -52,21 +53,25 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.currentRound = 0;
             this.winner = null;
+            this.logMessages = [];
         },
         attackMonster() {
             this.currentRound++
             const attackValue = getRandomValue(5, 12);
             this. monsterHealth -= attackValue;
+            this.addLogMessages('player', 'attack', attackValue);
             this.attackPlayer();
         },
         attackPlayer() {
             const attackValue = getRandomValue(8, 15);
             this. playerHealth -= attackValue; 
+            this.addLogMessages('monster', 'attack', attackValue);
         },
         specialAttackMonster() {
             this.currentRound++
             const attackValue =getRandomValue(10, 25);
             this.monsterHealth -= attackValue;
+            this.addLogMessages('player', 'special-attack', attackValue);
             this.attackPlayer();
         },
         healPlayer() {
@@ -77,7 +82,18 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue; 
             }
+            this.addLogMessages('player', 'heal', healValue);
             this.attackPlayer();
+        },
+        surrender() {
+            this.winner = 'monster';
+        },
+        addLogMessages(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            });
         }
     }
 }).mount('#game');
